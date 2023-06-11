9b0dea66f3b4d49cf1076671b84422bfcc812969
IMCH222 <chukwuim@mail.uc.edu>
2023-06-11 17:57:22 -0400

    Task 2 complete

--- src/BirthMonth_.java
+++ src/BirthMonth_.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class BirthMonth_ {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int birthMonth = 0;
+        int invalidMonth = 0;
+
+        System.out.print("Enter your birth month: ");
+
+        birthMonth = in.nextInt();
+        in.nextLine();
+
+        if((birthMonth >= 1) && (birthMonth < 12)){
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        else if((birthMonth < 1) || (birthMonth > 12)){
+            System.out.println("You entered an incorrect month value: " + birthMonth);
+        }
+
     }
 }
